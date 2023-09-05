630e2d6cc7f80732b05c828bb0113efb96c02b14
miguelvacuii <contacto@miguelmartinezmadrid.com>
2023-09-05 19:30:31 +0200

    01 - Buenas practicas - Modulo 1 - Ejercicio 5
    Crea un ejemplo para cada tipo: lista, tuplas y diccionarios

---
+++ Buenas practicas/Modulo 1 - C Sharp/Practica/Ejercicio5/Ejercicio5.cs
@@ -0,0 +1,17 @@
+using Internal;
+using Modulo1.Ejercicio5;
+
+namespace Modulo1.Ejercicio5
+{
+
+    // Lista
+    List<int> integerList = new List<int>();
+    integerList.Add(1);
+
+    // Tupla
+    Tuple<int, string, bool> tupleExample = new Tuple<int, string, bool>(1, "test", true);
+
+    // Diccionario
+    Dictionary<int, string> userPhones = new Dictionary<string, string>();
+    dict.Add("john Doe","555123456");
+}
