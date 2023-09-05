ccef65aa9d575c4d782e6286bd1f0cc21c6ff910
miguelvacuii <contacto@miguelmartinezmadrid.com>
2023-09-05 18:29:17 +0200

    01 - Buenas practicas - Modulo 1 - Ejercicio 2
    Crea dos ejemplos para cada tipo: constante y readonly

---
+++ Buenas practicas/Modulo 1 - C Sharp/Practica/Ejercicio2/Ejercicio2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class Ejercicio2
+    {
+        private readonly IRepository repository;
+        private readonly IMailer mailer;
+
+        private const int MIN_SIZE = 30;
+        private const int MAX_SIZE = 3000;
+    }
+}
